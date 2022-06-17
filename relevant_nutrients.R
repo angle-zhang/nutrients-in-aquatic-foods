3e8509a76ea3013211bdc10624e807dd80cf350a
angle-zhang <angelazhang1220@gmail.com>
2022-06-17 09:14:46 -0700

    added upper and lower bound

--- relevant_nutrients.R
+++ relevant_nutrients.R
@@ -11,7 +11,8 @@ afcd_raw = afcd_orig %>%
 ear_orig <- readRDS("data/GENUS_EAR_population_data_merged.Rds") 
 
 ear_us = ear_orig %>%
-  filter(iso3=="USA")
+  filter(iso3=="USA") %>%
+  mutate(ear_units=gsub("/d", "", units)) 
 
 # 1. do for one group first
 ear_us_g1 = ear_us %>%
@@ -20,8 +21,7 @@ ear_us_g1 = ear_us %>%
 # for each nutrient get the corresponding ear 
 nutrients = afcd_raw %>%
   merge(ear_us_g1, by="nutrient") %>% 
-  select(1:33, ear, units, sex, age) %>%
-  mutate(ear_units=gsub("/d", "", units)) %>%
+  select(1:33, ear, units, sex, age, ear_units) %>%
   # need to test this
   mutate(value=case_when(nutrient_units=="g" & ear_units=="mg" ~ value*1000, 
                          TRUE ~ value), # when species units is g and ear_units are mg
@@ -38,10 +38,39 @@ nutrients %>% mutate(ear_units=gsub("/d", "", units)) %>%
 
 # get median for all species
 median_nutrients = nutrients %>% group_by(sciname, nutrient, sex, age) %>%
-  summarize(median_ear=median(ear_proportion), median_ear_perc=median(ear_perc)) 
+  summarize(median_ear=median(ear_proportion), median_ear_perc=median(ear_perc))
+
+# get upper and lower bounds for all ear groups in the us
+ear_ul = ear_us %>% 
+  group_by(nutrient) %>%
+  mutate(
+    max_ear = max(ear),
+    min_ear = min(ear)
+  ) %>%
+  select(c(iso3, country, nutrient_type, nutrient, units, max_ear, min_ear, ear_units)) %>%
+  arrange(nutrient) %>%
+  unique()
+
+# get upper and lower bound 
+nutrients_ul = afcd_raw %>%
+  merge(ear_ul, by="nutrient") %>% 
+  select(1:33, units, min_ear, max_ear, ear_units) %>%
+  # need to test this
+  mutate(value=case_when(nutrient_units=="g" & ear_units=="mg" ~ value*1000, # TODO: move this to clean afcd_raw in the beginning
+                         TRUE ~ value), # when species units is g and ear_units are mg
+         nutrient_units=case_when(nutrient_units=="g" & ear_units=="mg" ~ "mg",
+                                  TRUE ~ nutrient_units)) %>% # change units
+  mutate(ear_proportion_lb = value/min_ear, 
+         ear_perc_lb = 100*value/min_ear,
+         ear_proportion_ub = value/max_ear, 
+         ear_perc_ub = 100*value/max_ear) 
+
 
 # export data 
 write.csv(median_nutrients,"output/median_nutrients.csv", row.names = FALSE)
+write.csv(nutrients_ul,"output/ub_nutrients.csv", row.names = FALSE)
+write.csv(nutrients,"output/nutrients.csv", row.names = FALSE)
+
 
 # divide value by EAR, RDA (global? , age groups?)
 
